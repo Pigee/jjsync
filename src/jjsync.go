0f87a6a79a489fdafa048da45d9195ea0e7d9fd2
Pigee <haiou511@gmail.com>
2016-10-18 14:28:07 +0800

    close connection directly to insure of procedure execution

--- src/jjsync.go
+++ src/jjsync.go
@@ -26,7 +26,7 @@ func main() {
 		return
 	}
 
-	connj, err := sql.Open("odbc", "dsn=jwater;uid=sa;pwd=qyKe2852274")
+	connj, err := sql.Open("odbc", "dsn=jwater;uid=sa;pwd=2852274")
 
 	if err != nil {
 		fmt.Println("%s:连接jjwater错误.%s\n", time.Now().Format("2006-01-02 15:04:05.000"), err)
@@ -34,33 +34,33 @@ func main() {
 	}
 	var wxTime string    //微信本次同步开始时间
 	var wxmaxTime string //微信本次同步截止时间
-	var bkTime string    //银行本次同步开始时间
-	var bkmaxTime string //银行本次同步截止时间
+	//var bkTime string    银行本次同步开始时间
+	//var bkmaxTime string //银行本次同步截止时间
 	//fmt.Printf("%d\n", os.Getpid())
 	cond := true
 	for cond {
 
 		wxTime = GetwxTime(connjj)
-		bkTime = GetbkTime(connjj)
+		//bkTime = GetbkTime(connjj)
 		wxmaxTime = SetwxmaxTime(connj, connjj)
-		bkmaxTime = SetbkmaxTime(connj, connjj)
+		//bkmaxTime = SetbkmaxTime(connj, connjj)
 		if wxTime == wxmaxTime {
-			fmt.Printf("%s:没有微信缴费数据更新....\n", time.Now().Format("2006-01-02 15:04:05.000"))
+			fmt.Printf("%s:本次共同步0笔微信缴费数据...................\n", time.Now().Format("2006-01-02 15:04:05.000"))
 		} else {
 			wx_row := SyncwxRecords(wxTime, wxmaxTime, connj, connjj)
-			fmt.Printf("%s:更新%d笔微信缴费数据....\n", time.Now().Format("2006-01-02 15:04:05.000"), wx_row)
-
+			fmt.Printf("%s:本次共同步%d笔微信缴费数据................\n", time.Now().Format("2006-01-02 15:04:05.000"), wx_row)
 		}
+		/*
 
-		if bkTime == bkmaxTime {
-			fmt.Printf("%s:没有银行缴费数据更新....\n", time.Now().Format("2006-01-02 15:04:05.000"))
-		} else {
-			bk_row := SyncbkRecords(bkTime, bkmaxTime, connj, connjj)
-			fmt.Printf("%s:更新%d条银行缴费数据....\n", time.Now().Format("2006-01-02 15:04:05.000"), bk_row)
-		}
-
-		fmt.Printf("%s:------------------------\n", time.Now().Format("2006-01-02 15:04:05.000"))
-		time.Sleep(30 * time.Second)
+			if bkTime == bkmaxTime {
+				fmt.Printf("%s:本次共同步0笔银行缴费数据.................\n", time.Now().Format("2006-01-02 15:04:05.000"))
+			} else {
+				bk_row := SyncbkRecords(bkTime, bkmaxTime, connj, connjj)
+				fmt.Printf("%s:本次共同步%d笔银行缴费数据...................\n", time.Now().Format("2006-01-02 15:04:05.000"), bk_row)
+			}
+		*/
+		fmt.Printf("%s:-----------------------------------------------\n", time.Now().Format("2006-01-02 15:04:05.000"))
+		time.Sleep(10 * time.Second)
 		//cond = false
 	}
 
@@ -73,22 +73,22 @@ func main() {
 //同步银行收费数据
 func SyncbkRecords(bkTime string, bkmaxTime string, connj *sql.DB, connjj *sql.DB) int {
 
-	stmt, err := connj.Prepare("select left(Yhdm,2),right(Yhdm,4), Lsh,  Kh, left(Fyrq,4),right(Fyrq,2),Fyrq, Dzrq, yjJe, sjJe, Wyj, Syys, Byys, Bz, CONVERT(VARCHAR(11),SFRQ,120)+SFSJ, kpbz from sfjl_sync where sync_date > ? and sync_date <= ?")
+	stmt, err := connj.Prepare("select Yhdm, Lsh,  Kh, cast(left(Fyrq,4) as int),cast(right(Fyrq,2) as int),Fyrq, Dzrq, yjJe, sjJe, Wyj, Syys, Byys, Bz, CONVERT(VARCHAR(11),SFRQ,120)+SFSJ, kpbz from sfjl_sync where sync_date > ? and sync_date <= ?")
 	// union all select ""maxtime"",max(sync_date) from sfjl_sync where sync_date > ?")
 
 	if err != nil {
-		fmt.Println("%s:Prepare SQL错误.%s\n", time.Now().Format("2006-01-02 15:04:05.000"), err)
+		fmt.Println("%s:Prepare bank SQL错误.%s\n", time.Now().Format("2006-01-02 15:04:05.000"), err)
 		return 0
 	}
 	defer stmt.Close()
 
 	row, err := stmt.Query(bkTime, bkmaxTime) // , mx, mx)
 	if err != nil {
-		fmt.Println("Query SQL错误:", err)
+		fmt.Println("Query bank SQL错误:", err)
 		return 0
 	}
 	var Yhdm string
-	var bkno string
+	//var bkno string
 	var Lsh string
 	var Kh string
 	var yea string
@@ -106,41 +106,24 @@ func SyncbkRecords(bkTime string, bkmaxTime string, connj *sql.DB, connjj *sql.D
 	r_count := 0
 	for row.Next() {
 
-		if err := row.Scan(&Yhdm, &bkno, &Lsh, &Kh, &yea, &mon, &Fyrq, &Dzrq, &yjJe, &sjJe, &Wyj, &Syys, &Byys, &Bz, &Sfsj, &kpbz); err == nil {
-			//fmt.Printf("%s, %s, %s,%s,%f\n", Yhdm, Lsh, Sfsj, Dzrq.Format("2006-01-02 15:04:05.000"), sjJe)
-			//更新帐单表FH_DEBTLIST
-			stmt_dblist, err := connjj.Prepare("UPDATE FH_DEBTLIST SET PAY_TAG = ?,PAY_WAY = ?,PAY_DATE = ?,BANK_PAYDATE = ?,DEBTL_ATOTAL = ?,DEBTL_ZNJ = ?,USERB_YHDM =?,BANK_NUMBER = ?,BANK_PAYQAN = ?,BANK_FLOWNO = ? WHERE USERB_KH =  ? AND DEBTL_YEAR = ? AND DEBTL_MON = ?")
-			if err != nil {
-				fmt.Println("%s:Prepare SQL错误.%s\n", time.Now().Format("2006-01-02 15:04:05.000"), err)
-				return 0
-			}
-			stmt_dblist.Query("2", "2", time.Now(), Sfsj, sjJe+Wyj, Wyj, Yhdm, bkno, sjJe+Wyj, Lsh, Kh, yea, mon)
+		if err := row.Scan(&Yhdm, &Lsh, &Kh, &yea, &mon, &Fyrq, &Dzrq, &yjJe, &sjJe, &Wyj, &Syys, &Byys, &Bz, &Sfsj, &kpbz); err == nil {
 
-			defer stmt_dblist.Close()
-			//更新帐单表FH_DEBTOWNHIS
-			stmt_ownhis, err := connjj.Prepare("UPDATE FH_DEBTOWNHIS SET PAY_TAG = ?,PAY_WAY = ?,PAY_DATE = ?,BANK_PAYDATE = ?,DEBTL_ATOTAL = ?,DEBTL_ZNJ = ?,USERB_YHDM =?,BANK_NUMBER = ?,BANK_PAYQAN = ?,BANK_FLOWNO = ? WHERE USERB_KH =  ? AND DEBTL_YEAR = ? AND DEBTL_MON = ?")
-			if err != nil {
-				fmt.Println("%s:Prepare SQL错误.%s\n", time.Now().Format("2006-01-02 15:04:05.000"), err)
-				return 0
-			}
-			stmt_ownhis.Query("2", "2", time.Now(), Sfsj, sjJe+Wyj, Wyj, Yhdm, bkno, sjJe+Wyj, Lsh, Kh, yea, mon)
+			//fmt.Println(Yhdm, "--", yea, "--", mon, "NOrecords")
+			//调用计费存储过程
+			stmt_sp, err := connjj.Prepare("EXEC SP_BANK_SINGLEPAYC ?,?,?,?,?,?,?")
 
-			defer stmt_ownhis.Close()
-
-			//插入记录到FH_FGTLIST
-			stmt_fgt, err := connjj.Prepare("INSERT INTO [FH_FGTLIST] ([FGTLIST_ID], [FGTFLOW_NO], [USERB_KH],  [FGT_YEAR],  [FGT_STOTAL], [FGT_ATOTAL], [FGT_WYJ], [FGT_LLEFT], [FGT_BLEFT], [FGT_TAG], [FGT_CZTAG], [FGT_SFDATE], [CREATE_PERSON], [CREATE_DATE], [UPDATE_PERSON], [UPDATE_DATE], [FGT_WAY], [FGT_MONTH], [USERB_YHDM], [BANK_NUMBER], [CZFLOW_NO], [PAY_DATE])  SELECT NEWID(),?,?,?,?,?,?,?,?,?,?,?,?,GETDATE(),NULL,NULL,?,?,?,?,NULL,GETDATE()")
 			if err != nil {
 				fmt.Println("%s:Prepare SQL错误.%s\n", time.Now().Format("2006-01-02 15:04:05.000"), err)
 				return 0
 			}
 
-			stmt_fgt.Query(Lsh, Kh, yea, yjJe, sjJe+Wyj, Wyj, Byys-Syys, 0.0, "0", "0", Sfsj, "1", "2", mon, Yhdm, bkno)
+			stmt_sp.Query(Kh, Yhdm, Lsh, Sfsj, sjJe+Wyj, yea, mon) // , mx, mx)
 
-			defer stmt_fgt.Close()
+			stmt_sp.Close() //显式的关闭此连接，保证在存储过程调用执行完成后，才进入下一轮循环
+
+			fmt.Printf("%s:同步卡号%s流水号%s账单金额%f完成.\n", time.Now().Format("2006-01-02 15:04:05.000"), Kh, Lsh, sjJe+Wyj)
+
 			r_count++
-			//INSERT INTO [FH_FGTLIST] ([FGTLIST_ID], [FGTFLOW_NO], [USERB_KH],  [FGT_YEAR],  [FGT_STOTAL], [FGT_ATOTAL], [FGT_WYJ], [FGT_LLEFT], [FGT_BLEFT], [FGT_TAG], [FGT_CZTAG], [FGT_SFDATE], [CREATE_PERSON], [CREATE_DATE], [UPDATE_PERSON], [UPDATE_DATE], [FGT_WAY], [FGT_MONTH], [USERB_YHDM], [BANK_NUMBER], [CZFLOW_NO], [PAY_DATE])  SELECT NEWID(),@BK_FLOWNO,@UB_KH,@VAR_Y,@VAR_TOTAL-@VAR_ZNJ,@VAR_TOTAL,@VAR_ZNJ,@UB_YE,0.0,'0','0',CAST(@SF_DATE AS DATETIME),'1',GETDATE(),NULL,NULL,'2',@VAR_M,LEFT(@BK_NO,2),RIGHT(@BK_NO,4),NULL,GETDATE();
-			//defer row.Close()
-
 		}
 	}
 	return r_count
@@ -153,14 +136,14 @@ func SyncwxRecords(wxTime string, wxmaxTime string, connj *sql.DB, connjj *sql.D
 	// union all select ""maxtime"",max(sync_date) from sfjl_sync where sync_date > ?")
 
 	if err != nil {
-		fmt.Println("%s:Prepare SQL错误.%s\n", time.Now().Format("2006-01-02 15:04:05.000"), err)
+		fmt.Println("%s:Prepare weixin SQL错误.%s\n", time.Now().Format("2006-01-02 15:04:05.000"), err)
 		return 0
 	}
 	defer stmt.Close()
 
 	row, err := stmt.Query(wxTime, wxmaxTime) // , mx, mx)
 	if err != nil {
-		fmt.Println("Query SQL错误:", err)
+		fmt.Println("Query weixin SQL错误:", err)
 		return 0
 	}
 
@@ -181,11 +164,16 @@ func SyncwxRecords(wxTime string, wxmaxTime string, connj *sql.DB, connjj *sql.D
 	var DT_DEAL_MONTH int
 	var DT_DEAL_MONEY float64
 	var DT_N_ZNJ float64
+
+	//var DT_WATERP_QAN int
+	//var DT_USERB_SFFS string
 	r_count := 0
 	for row.Next() {
 
 		if err := row.Scan(&DEAL_ID, &DEAL_NO, &CUST_KH, &DEAL_MONEY, &DEAL_DATE, &CREATE_DATE, &STATUS, &WX_NO, &SS_MONEY); err == nil {
-			//同步FH_WEINXINDEAL数据
+
+			//fmt.Println(DEAL_NO)
+
 			stmt, err := connjj.Prepare("insert into fh_weixindeal values (?,?,?,?,?,?,?,?,?)")
 			// union all select ""maxtime"",max(sync_date) from sfjl_sync where sync_date > ?")
 
@@ -198,7 +186,7 @@ func SyncwxRecords(wxTime string, wxmaxTime string, connj *sql.DB, connjj *sql.D
 			stmt.Query(DEAL_ID, DEAL_NO, CUST_KH, DEAL_MONEY, nil, CREATE_DATE, "0", nil, nil)
 			//获得FH_WXDEALDETAIL数据
 
-			stmt_dt, err := connj.Prepare("select * from  fh_wxdealdetail where deal_no = ?")
+			stmt_dt, err := connj.Prepare("select [DETAIL_ID], [DEAL_NO], [CUST_KH], [DEAL_YEAR], [DEAL_MONTH], [DEAL_MONEY], [N_ZNJ] from  fh_wxdealdetail where deal_no = ?")
 
 			if err != nil {
 				fmt.Println("%s:Prepare SQL错误.%s\n", time.Now().Format("2006-01-02 15:04:05.000"), err)
@@ -210,11 +198,12 @@ func SyncwxRecords(wxTime string, wxmaxTime string, connj *sql.DB, connjj *sql.D
 				fmt.Println("Query SQL错误:", err)
 				return 0
 			}
+
 			for row_dt.Next() {
 
 				if err := row_dt.Scan(&DT_DETAIL_ID, &DT_DEAL_NO, &DT_CUST_KH, &DT_DEAL_YEAR, &DT_DEAL_MONTH, &DT_DEAL_MONEY, &DT_N_ZNJ); err == nil {
 					//同步FH_WXDEALDETAIL数据
-					stmt_indt, err := connjj.Prepare("insert into fh_wxdealdetail values (?,?,?,?,?,?,?)")
+					stmt_indt, err := connjj.Prepare("insert into fh_wxdealdetail([DETAIL_ID], [DEAL_NO], [CUST_KH], [DEAL_YEAR], [DEAL_MONTH], [DEAL_MONEY], [N_ZNJ]) values (?,?,?,?,?,?,?)")
 
 					if err != nil {
 						fmt.Println("%s:Prepare SQL错误.%s\n", time.Now().Format("2006-01-02 15:04:05.000"), err)
@@ -229,6 +218,7 @@ func SyncwxRecords(wxTime string, wxmaxTime string, connj *sql.DB, connjj *sql.D
 			}
 
 			//调用计费存储过程
+
 			stmt_sp, err := connjj.Prepare("EXEC WX_DEBT_PAYC ?,?,?")
 
 			if err != nil {
@@ -236,15 +226,18 @@ func SyncwxRecords(wxTime string, wxmaxTime string, connj *sql.DB, connjj *sql.D
 				return 0
 			}
 
-			_, stmtRowsErr := stmt_sp.Query(DEAL_NO, WX_NO, DEAL_MONEY) // , mx, mx)
+			//fmt.Println(connjj.Stats())
+			//time.Sleep(5 * time.Second)
+			_, err = stmt_sp.Exec(DEAL_NO, WX_NO, DEAL_MONEY)
 
-			// stmt.Query("AB1234")
+			if err != nil {
+				fmt.Println("%s:执行过程错误.%s\n", time.Now().Format("2006-01-02 15:04:05.000"), err)
 
-			if stmtRowsErr != nil {
-				fmt.Printf("\nstmtRowsErr: %s\n", stmtRowsErr)
 			}
 
-			defer stmt_sp.Close()
+			fmt.Printf("%s:同步卡号%s流水号%s账单金额%f完成.\n", time.Now().Format("2006-01-02 15:04:05.000"), CUST_KH, DEAL_NO, DEAL_MONEY)
+
+			stmt_sp.Close() //显式的关闭此连接，保证在存储过程调用执行完成后，才进入下一轮循环
 
 		}
 		r_count++
